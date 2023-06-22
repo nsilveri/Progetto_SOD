bae2d57b4582c2a8f861b41f28665da4cb63fb8e
nsilveri <nicola.silveri@hotmail.it>
2023-06-22 15:01:54 +0200

    Changes to use sensors on I2C1

--- Arduino/Pico_SOD/Pico_SOD.ino
+++ Arduino/Pico_SOD/Pico_SOD.ino
@@ -9,8 +9,10 @@
 //INCLUDERE FREERTOS==================================================================================
 
 ArtronShop_BH1750 bh1750(0x23, &Wire1); // Non Jump ADDR: 0x23, Jump ADDR: 0x5C
-Adafruit_BMP280 bmp; // I2C
-RTC_PCF8523 rtc;
+Adafruit_BMP280 bmp(&Wire1); // I2C1
+RTC_PCF8523 rtc;//(&Wire1); NECESSARIA MODIFICA DELLA LIBRERIA PER I2C1
+
+
 
 void BMP280_setup()
 {
@@ -41,7 +43,7 @@ void BMP280_setup()
 
 void PCF8523_setup()
 {
-  if (! rtc.begin()) {
+  if (! rtc.begin(&Wire1)) {
     Serial.println("Couldn't find RTC");
     Serial.flush();
     while (1) delay(10);
@@ -49,50 +51,17 @@ void PCF8523_setup()
 
   if (! rtc.initialized() || rtc.lostPower()) {
     Serial.println("RTC is NOT initialized, let's set the time!");
-    // When time needs to be set on a new device, or after a power loss, the
-    // following line sets the RTC to the date & time this sketch was compiled
+
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-    //
-    // Note: allow 2 seconds after inserting battery or applying external power
-    // without battery before calling adjust(). This gives the PCF8523's
-    // crystal oscillator time to stabilize. If you call adjust() very quickly
-    // after the RTC is powered, lostPower() may still return true.
   }
 
-  // When time needs to be re-set on a previously configured device, the
-  // following line sets the RTC to the date & time this sketch was compiled
-  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  // This line sets the RTC with an explicit date & time, for example to set
-  // January 21, 2014 at 3am you would call:
-  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-
-  // When the RTC was stopped and stays connected to the battery, it has
-  // to be restarted by clearing the STOP bit. Let's do this to ensure
-  // the RTC is running.
   rtc.start();
-
-   // The PCF8523 can be calibrated for:
-  //        - Aging adjustment
-  //        - Temperature compensation
-  //        - Accuracy tuning
-  // The offset mode to use, once every two hours or once every minute.
-  // The offset Offset value from -64 to +63. See the Application Note for calculation of offset values.
-  // https://www.nxp.com/docs/en/application-note/AN11247.pdf
-  // The deviation in parts per million can be calculated over a period of observation. Both the drift (which can be negative)
-  // and the observation period must be in seconds. For accuracy the variation should be observed over about 1 week.
-  // Note: any previous calibration should cancelled prior to any new observation period.
-  // Example - RTC gaining 43 seconds in 1 week
   float drift = 43; // seconds plus or minus over oservation period - set to 0 to cancel previous calibration.
   float period_sec = (7 * 86400);  // total obsevation period in seconds (86400 = seconds in 1 day:  7 days = (7 * 86400) seconds )
   float deviation_ppm = (drift / period_sec * 1000000); //  deviation in parts per million (μs)
   float drift_unit = 4.34; // use with offset mode PCF8523_TwoHours
   // float drift_unit = 4.069; //For corrections every min the drift_unit is 4.069 ppm (use with offset mode PCF8523_OneMinute)
   int offset = round(deviation_ppm / drift_unit);
-  // rtc.calibrate(PCF8523_TwoHours, offset); // Un-comment to perform calibration once drift (seconds) and observation period (seconds) are correct
-  // rtc.calibrate(PCF8523_TwoHours, 0); // Un-comment to cancel previous calibration
 
   Serial.print("Offset is "); Serial.println(offset); // Print to control offset
 
@@ -100,7 +69,7 @@ void PCF8523_setup()
 
 void BH1750_setup()
 {
-    while (!bh1750.begin()) {
+    if (!bh1750.begin()) {
       Serial.println("BH1750 not found !");
       delay(1000);
   }
@@ -118,56 +87,14 @@ void setup() {
   Wire1.setSCL(7);
   Wire1.begin();
   
-  //BH1750_setup();   //Setup sensore BH1750
-  //PCF8523_setup();  //Setup sensore PCF8523
-  //BMP280_setup();   //Setup sensore BMP280
+  BH1750_setup();   //Setup sensore BH1750
+  PCF8523_setup();  //Setup sensore PCF8523
+  BMP280_setup();   //Setup sensore BMP280
 }
 
 
 
 float PCF8523_data_read(){
-    
-    /* CODICE ORIGINALE LIBRERIA
-    DateTime now = rtc.now();
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(' ');
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
-    
-    Serial.print(" since midnight 1/1/1970 = ");
-    Serial.print(now.unixtime());
-    Serial.print("s = ");
-    Serial.print(now.unixtime() / 86400L);
-    Serial.println("d");
-    
-    // calculate a date which is 7 days and 30 seconds into the future
-    DateTime future (now.unixtime() + 7 * 86400L + 30);
-    
-    Serial.print(" now + 7d + 30s: ");
-    Serial.print(future.year(), DEC);
-    Serial.print('/');
-    Serial.print(future.month(), DEC);
-    Serial.print('/');
-    Serial.print(future.day(), DEC);
-    Serial.print(' ');
-    Serial.print(future.hour(), DEC);
-    Serial.print(':');
-    Serial.print(future.minute(), DEC);
-    Serial.print(':');
-    Serial.print(future.second(), DEC);
-    Serial.println();
-    
-    Serial.println();
-    delay(3000);
-    */
 
   DateTime now = rtc.now();
   //String ts = String(now);
@@ -178,35 +105,12 @@ float PCF8523_data_read(){
 
 float BH1750_data_read() 
 {
-/* CODICE ORIGINALE DELLA LIBRERIA
-  Serial.print("Light: ");
-  Serial.print(bh1750.light());
-  Serial.print(" lx");
-  Serial.println();
-  delay(1000);
-*/
   float lux_level = bh1750.light();
   return lux_level;
 }
 
 String BMP280_data_read()
 {
-  /* CODICE ORIGINALE LIBRERIA
-  Serial.print(F("Temperature = "));
-  Serial.print(bmp.readTemperature());
-  Serial.println(" *C");
-
-  Serial.print(F("Pressure = "));
-  Serial.print(bmp.readPressure());
-  Serial.println(" Pa");
-
-  Serial.print(F("Approx altitude = "));
-  Serial.print(bmp.readAltitude(1013.25)); // Adjusted to local forecast!
-  Serial.println(" m");
-
-  Serial.println();
-  delay(2000);
-  */
 
  float temp = bmp.readTemperature();
  float bar =  bmp.readPressure();
@@ -221,8 +125,8 @@ void loop() //SOLO PER TEST, CON FREERTOS QUESTO VA VIA
 {
   String alt_value = BMP280_data_read();
   float lux = BH1750_data_read();
-  int timestamp = PCF8523_data_read();
+  int timestamp = millis();
 
-  String sensors_test = String("alt_value: " + alt_value + "| lux: " + String(lux))+ "| TimeStamp: " + timestamp);
+  String sensors_test = String("alt_value: " + alt_value + "| lux: " + String(lux) + "| TimeStamp: " + String(timestamp));
 
 }
