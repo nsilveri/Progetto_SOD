d067f7d7d1682aa5dced6f1d20a5179064ceaa08
nsilveri <nicola.silveri@hotmail.it>
2023-06-23 02:59:54 +0200

    A lot of changes
    Added FreeRTOS, sensors tasks, Pico I2C Slave task, semaphores.

--- Arduino/Pico_SOD/Pico_SOD.ino
+++ Arduino/Pico_SOD/Pico_SOD.ino
@@ -1,16 +1,50 @@
+//Librerie Arduino
 #include <Arduino.h>
 #include <Wire.h>
+#include <ArduinoJson.h>
+
+//Librerie FreeRTOS
 #include <FreeRTOS.h>
+#include <task.h>
+#include <semphr.h>
+
+//Librerie Sensori
 #include <ArtronShop_BH1750.h>
 #include "RTClib.h"
 #include <Adafruit_BMP280.h>
 
+#define SLAVE_ADDRESS 0x08 // Indirizzo della Pico in modalità Slave sull'I2C0
 
-//INCLUDERE FREERTOS==================================================================================
-
+//Definizione sensori
 ArtronShop_BH1750 bh1750(0x23, &Wire1); // Non Jump ADDR: 0x23, Jump ADDR: 0x5C
 Adafruit_BMP280 bmp(&Wire1); // I2C1
 RTC_PCF8523 rtc;// I2C1 set in setup()
+
+//Variabili globali dei dati ottenuti dai sensori
+String BMP280_data = "";
+float BH1750_data = 0.0;
+unsigned long PCF8523_data = 0;
+
+//Variabile globale del comando ottenuti dalla Raspberry Pi
+String Received_Comand = "";
+
+String Requested_Data = "";
+
+//Semafori delle variabili globali dei dati dei sensori
+/*
+Le variabili globali saranno scritte dai Task dei singoli sensori e saranno lette dal Task che gestisce l'I2C Slave
+Per cui necessita dei semafori
+*/
+SemaphoreHandle_t BMP280_data_Mutex;
+SemaphoreHandle_t BH1750_data_Mutex;
+SemaphoreHandle_t PCF8523_data_Mutex;
+SemaphoreHandle_t I2C0_Mutex;
+
+// Prototipi delle funzioni
+void BMP280(void *params);
+void BH1750(void *params);
+void PCF8523(void *params);
+void I2C_Slave_Client(void *params);
 
 void BMP280_setup()
 {
@@ -75,16 +109,64 @@ void BH1750_setup()
 
 void setup() {
   Serial.begin(115200);
-  //Definizione PIN ed inizializzazione I2C0
+  //Definizione PIN ed inizializzazione I2C0 in Slave
   Wire.setSDA(4);
   Wire.setSCL(5);
-  Wire.begin();
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
 
-  //Definizione PIN ed inizializzazione I2C1
+  //Definizione PIN ed inizializzazione I2C1 in Master
   Wire1.setSDA(6);
   Wire1.setSCL(7);
   Wire1.begin();
-  /*
+
+  //Creazione Task BMP280
+  xTaskCreate(
+  BMP280,  //void da eseguire 
+  "BMP280",   // nome processo
+  128,        // dimensione stack (byte)
+  NULL,       // parametri
+  1,          // priorità (più è alto, maggiore è la priorità)
+  NULL        // riferimento per accedere al task dall'esterno
+  );
+
+  //Creazione Task BH1750
+  xTaskCreate(
+  BH1750,  //void da eseguire 
+  "BH1750",   // nome processo
+  128,        // dimensione stack (byte)
+  NULL,       // parametri
+  1,          // priorità (più è alto, maggiore è la priorità)
+  NULL        // riferimento per accedere al task dall'esterno
+  );
+
+
+  //Creazione Task PCF8523
+  xTaskCreate(
+  PCF8523,    //void da eseguire 
+  "PCF8523",  // nome processo
+  128,        // dimensione stack (byte)
+  NULL,       // parametri
+  1,          // priorità (più è alto, maggiore è la priorità)
+  NULL        // riferimento per accedere al task dall'esterno
+  );
+
+  //Creazione Task I2C_Slave_Client
+  xTaskCreate(
+  I2C_Slave_Client,     //void da eseguire 
+  "I2C_Slave_Client",   // nome processo
+  128,                  // dimensione stack (byte)
+  NULL,                 // parametri
+  1,                    // priorità (più è alto, maggiore è la priorità)
+  NULL                  // riferimento per accedere al task dall'esterno
+  );
+
+  //Avvio scheduler di FreeRTOS
+  vTaskStartScheduler();
+
+  I2C0_Mutex = xSemaphoreCreateMutex();
+  /* Non necessari con FreeRTOS (ogni task ha il suo setup())
   BH1750_setup();   //Setup sensore BH1750
   PCF8523_setup();  //Setup sensore PCF8523
   BMP280_setup();   //Setup sensore BMP280
@@ -92,7 +174,140 @@ void setup() {
 
 }
 
+//Task BMP280
+void BMP280(void *params){
+/*setup()*/
 
+//PCF8523_setup(); //da decommentare quando avremo i sensori
+
+/*loop()*/
+while (true)
+{
+  //BMP280_data_read(); //da decommentare quando avremo i sensori
+  if(xSemaphoreTake(I2C0_Mutex, portMAX_DELAY) == pdTRUE)
+  {
+    if (xSemaphoreTake(BMP280_data_Mutex, portMAX_DELAY) == pdTRUE)
+      {
+        BMP280_data = String(random(0,50)) + "," + String(random(500, 2000)) + "," + String(random(0, 10000));
+        xSemaphoreGive(BMP280_data_Mutex);
+      }
+    xSemaphoreGive(I2C0_Mutex);
+  }
+  vTaskDelay(1000 / portTICK_PERIOD_MS); //delay gestito da FreeRTOS
+}
+
+
+}
+
+//Task BH1750
+void BH1750(void *params){
+/*setup()*/
+
+//BH1750_setup(); //da decommentare quando avremo i sensori
+
+/*loop()*/
+  while (true)
+  {
+    //BH1750_data_read();//da decommentare quando avremo i sensori
+    if(xSemaphoreTake(I2C0_Mutex, portMAX_DELAY) == pdTRUE)
+    {
+      if (xSemaphoreTake(BH1750_data_Mutex, portMAX_DELAY) == pdTRUE)
+      {
+        BH1750_data =  random(0.0,1.1);
+        xSemaphoreGive(BH1750_data_Mutex);
+      }
+      xSemaphoreGive(I2C0_Mutex);
+    }
+    vTaskDelay(1000 / portTICK_PERIOD_MS); //delay gestito da FreeRTOS
+  }
+}
+
+//Task PCF8523
+void PCF8523(void *params){
+/*setup()*/
+
+  //PCF8523_setup(); //da decommentare quando avremo i sensori
+
+/*loop()*/
+  while (true)
+  {
+    if(xSemaphoreTake(I2C0_Mutex, portMAX_DELAY) == pdTRUE)
+    {
+      //PCF8523_data_read(); //da decommentare quando avremo i sensori
+      if (xSemaphoreTake(PCF8523_data_Mutex, portMAX_DELAY) == pdTRUE)
+      {
+        PCF8523_data = millis();
+        xSemaphoreGive(PCF8523_data_Mutex);
+      }
+      xSemaphoreGive(I2C0_Mutex);
+    }
+    vTaskDelay(1000 / portTICK_PERIOD_MS); //delay gestito da FreeRTOS
+  }
+}
+
+//CallBack, parte in automatico quando ci sono richieste sul'I2C0
+void receiveData(int byteCount) {
+  while (Wire.available()) {
+    char data = Wire.read();
+    // Elabora i dati ricevuti come necessario
+    Serial.print("Comando ricevuto: ");
+    Serial.println(data);
+    Received_Comand += data;
+  }
+}
+
+void sendData() {
+  for (size_t i = 0; i < Requested_Data.length(); i++) {
+    Wire.write(Requested_Data[i]);
+  Requested_Data = "";
+  }
+}
+
+String generateJsonString(String name, String data, String timestamp) {
+
+  StaticJsonDocument<200> doc;
+  doc["name"] = name;
+  doc["data"] = data;
+  doc["timestamp"] = timestamp;
+
+  String jsonString;
+  serializeJson(doc, jsonString);
+
+  return jsonString;
+}
+
+void I2C_Slave_Client(void *params){
+/*setup()*/
+ 
+/*loop()*/
+  while (true){
+    if(Received_Comand == "0")
+    {
+      xSemaphoreTake(BH1750_data_Mutex, portMAX_DELAY);
+      String jsonString = generateJsonString("BH1750", String(BH1750_data), String(millis()));
+      Requested_Data = jsonString;
+      //Requested_Data = String("BH1750" + String(BH1750_data) + "," + String(millis()));
+      xSemaphoreGive(BH1750_data_Mutex);
+
+    }else if(Received_Comand == "1")
+    {
+      xSemaphoreTake(BMP280_data_Mutex, portMAX_DELAY);
+      String jsonString = generateJsonString("BMP280", String(BMP280_data), String(millis()));
+      Requested_Data = jsonString;
+      //Requested_Data = String("BH1750" + String(BH1750_data) + "," + String(millis()));
+      xSemaphoreGive(BMP280_data_Mutex);
+
+    }else if(Received_Comand == "2")
+    {
+      xSemaphoreTake(PCF8523_data_Mutex, portMAX_DELAY);
+      String jsonString = generateJsonString("PCF8523", String(PCF8523_data), String(millis()));
+      Requested_Data = jsonString;
+      //Requested_Data = String("PCF8523" + String(PCF8523_data) + "," + String(millis()));
+      xSemaphoreGive(PCF8523_data_Mutex); 
+
+    }
+  }
+}
 
 float PCF8523_data_read(){
 
@@ -121,13 +336,16 @@ String BMP280_data_read()
  return sensor_data;
 }
 
-void loop() //SOLO PER TEST, CON FREERTOS QUESTO VA VIA
+void loop(){}
+/*
+  void loop() //SOLO PER TEST, CON FREERTOS QUESTO VA VIA
 {
 /*
   String alt_value = BMP280_data_read();
   float lux = BH1750_data_read();
   int timestamp = millis();
 */
+/*
   String alt_value = String(random(0,50)) + "," + String(random(500, 2000)) + "," + String(random(0, 10000)); //random(min, max)
   float lux = random(0.0,1.1);
   int timestamp = millis();
@@ -136,3 +354,7 @@ void loop() //SOLO PER TEST, CON FREERTOS QUESTO VA VIA
   String sensors_test = String("alt_value: " + alt_value + "| lux: " + String(lux) + "| TimeStamp: " + String(timestamp));
 
 }
+*/
+
+
+
